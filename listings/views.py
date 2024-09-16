bc30f269d1e0e05ef9860a8baadadc6fd4bd3f42
hamid.bajwa.2865 <hamid.bajwa.2865@slashnext.com>
2024-09-16 23:16:14 +0500

    Listings: initial commit

---
+++ listings/views.py
@@ -0,0 +1,12 @@
+'''
+---------------------------------------------------
+Project:        Braelo
+Date:           Aug 14, 2024
+Author:         Hamid
+---------------------------------------------------
+
+Description:
+End points file (abstracted).
+All end points will be in Braelo/listings/api/
+---------------------------------------------------
+'''
